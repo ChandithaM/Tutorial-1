5ba346373b966dca17a22a25a43fa443b24fcf7c
ChandithaM <19084608@student.westernsydney.edu.au>
2017-07-26 19:23:25 +1000

    Bug fixes

--- Task1.6.cpp
+++ Task1.6.cpp
@@ -7,25 +7,37 @@ using namespace std;
 
 int main() {
 
+	//initializing variables
     char run;
     int randN;
     int guessN;
+
+    //Startup Banner
     cout << "Welcome to Guess the Number!!!\n";
+    cout << "--------------------------------------------\n";
 
+    //do loop to always run game the first time
+    do
 
-    do{
+    {
 
+    	//getting a new random number every round
         srand(time(NULL));
         randN = (rand()%15) +1;
 
+        //for loop to limit game to 3 attempts
         for (int i = 0; i < 3; ++i) {
+
+			//request and input for guessing game
             cout << "\nGuess the SECRET number: ";
             cin >> guessN;
 
+            //condition for winning game
             if (guessN == randN){
                 cout << "That's correct, you Win!\n";
                 break;
             }
+
 
             if (i ==2){
                 cout << "Oops you're out of tries. Game Over.";
@@ -44,6 +56,9 @@ int main() {
         cin >> run;
 
 
-    }while(run == 'Y' || 'y');
+    }while(run == 'Y' || run == 'y');
+
+    cout << "\nThanks for playing. Bye!";
+
     return 0;
 }
